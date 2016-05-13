8516a6d3cf84d63be7bd21014fd0c150965b7df3
Deyvis Valdez <deyvis.vh@gmail.com>
2016-05-13 18:42:29 -0500

    EventEmitter instead callbacks

--- server.js
+++ server.js
@@ -3,7 +3,12 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
 
-const server = http.createServer(onRequest);
+const server = http.createServer();
+
+server.on('request', onRequest);
+server.on('listening', onListening);
+
+server.listen(port);
 
 function onRequest (req, res){
     res.end('Hello Node.js');
@@ -13,4 +18,3 @@ function onListening(){
     console.log('Server listens in port '+port);
 }
 
-server.listen(port, onListening);
