a296e2b047fdb822e23571e9143042a230a3d7ef
Deyvis Valdez <deyvis.vh@gmail.com>
2016-05-13 20:35:28 -0500

    serve js file

--- server.js
+++ server.js
@@ -13,13 +13,42 @@ server.on('listening', onListening);
 server.listen(port);
 
 function onRequest (req, res){
+    let uri = req.url;
+
+    if (uri.startsWith('/index') || uri === '/') {
+        return serveIndex(res);
+    }
+
+    if (uri === '/app.js') {
+        return serveApp(res);
+    }
+
+    res.statusCode = 404;
+    res.end(`404 not found: ${uri}`);
+}
+
+function serveIndex(res){
     let index = path.join(__dirname, 'public', 'index.html');
+    let rs = fs.createReadStream(index);
 
     res.setHeader('Content-Type', 'text/html');
-    let rs = fs.createReadStream(index);
     rs.pipe(res);
 
     rs.on('error', function (err){
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(err.message);
+    })
+}
+
+function serveApp(res){
+    let app = path.join(__dirname, 'public', 'app.js');
+    let rs = fs.createReadStream(app);
+
+    res.setHeader('Content-Type', 'text/javascript');
+    rs.pipe(res);
+
+    rs.on('error', function (err){
+        res.setHeader('Content-Type', 'text/plain');
         res.end(err.message);
     })
 }
